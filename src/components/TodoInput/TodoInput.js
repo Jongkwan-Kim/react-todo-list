e9230f7cce0b067baa36e7d755f2e7ac45023a9f
jgkim <jgkim@mocainc.com>
2020-10-19 13:44:55 +0900

    setup project

---
+++ src/components/TodoInput/TodoInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './TodoInput.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const TodoInput = ({ value, onChange, onInsert }) => {
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            onInsert();
+        }
+    }
+
+    return (
+        <div className={cx('todo-input')}>
+            <input type="text" onChange={onChange} value={value} onKeyPress={onkeypress}/>
+            <div className={cx('add-button')} onClick={onInsert}>Add</div>
+        </div>
+    );
+};
+
+export default TodoInput;
